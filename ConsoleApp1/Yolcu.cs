b6e13ae7569c0e1e0e03e2e23ba305a502e41ec1
Emre Karay <emrekaray666@gmail.com>
2022-02-11 02:21:41 +0300

    C# KALITIM ÖRNEĞİ

---
+++ ConsoleApp1/Yolcu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Yolcu
+    {
+        private string ad;
+        private string soyad;
+        private int yas;
+        private string cinsiyet;
+
+        public string ADI
+        {
+            get { return ad; }
+            set { ad = value.ToLower(); }
+        }
+        public string SOYADI
+        {
+            get { return soyad; }
+            set { soyad = value.ToUpper(); }
+        }
+        public int YAS
+        {
+            get { return yas; }
+            set { yas = Math.Abs(value); }
+        }
+        public string CİNSİYET
+        {
+            get { return cinsiyet; }
+            set { cinsiyet = value.ToLower(); }
+        }
+    }
+}
