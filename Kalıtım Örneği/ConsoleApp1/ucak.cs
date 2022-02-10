36d354f639ffb95f8288391c52e5be50f4472f56
Emre Karay <81099349+emrekaray@users.noreply.github.com>
2022-02-11 02:26:12 +0300

    C# KALITIM ÖRNEĞİM

---
+++ Kalıtım Örneği/ConsoleApp1/ucak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class ucak : Yolcu  //KALITIM YAPMA YOLU
+    {
+        private string marka;
+        private string kalkis;
+        private string varis;
+
+        public string MARKA
+        {
+            get { return marka; }
+            set { marka = value.ToUpper(); }
+        }
+        public string KALKIS
+        {
+            get { return kalkis; }
+            set { kalkis = value.ToLower(); }
+        }
+        public string VARIS
+        {
+            get { return varis; }
+            set { varis = value.ToUpper(); }
+        }
+
+    }
+}
