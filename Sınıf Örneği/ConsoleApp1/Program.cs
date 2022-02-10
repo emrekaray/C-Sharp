f5017c607ab5c2fbf74a20e241c255f2c54fca53
Emre Karay <81099349+emrekaray@users.noreply.github.com>
2022-02-11 02:29:16 +0300

    C# SINIF ÖRNEĞİ

---
+++ Sınıf Örneği/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KAPSULLEME
+            emlak eml = new emlak();
+            eml.SEMT = "Kadıköy";
+            eml.RENGİ = "mavi";
+            eml.ODASAYİSİ = 4;
+            eml.KATNUMARA = 8;
+            eml.ALAN = 80;
+
+            Console.WriteLine("Evin Bulunduğu Semt : "+ eml.SEMT);
+            Console.WriteLine("Evin Oda Renkleri : "+ eml.RENGİ);
+            Console.WriteLine("Evin Toplam Oda Sayısı : "+ eml.ODASAYİSİ);
+            Console.WriteLine("Evin Bulunduğu Kat : "+eml.KATNUMARA);
+            Console.WriteLine("Evin Toplam Alanı : "+eml.ALAN + " Metrekare");
+        }
+    }
+}
---
+++ Sınıf Örneği/ConsoleApp1/emlak.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class emlak
+    {
+
+        private string semt;
+        private string renk;
+        private int odasayisi;
+        private int katno;
+        private double alan;
+
+        public string SEMT
+        {
+            get { return semt; }
+            set { semt = value.ToUpper(); }
+            
+        }
+        public string RENGİ
+        {
+            get { return renk; }
+            set { renk = value.ToLower(); }
+        }
+
+        public int ODASAYİSİ
+        {
+            get { return odasayisi; }
+            set { odasayisi = Math.Abs(value); }
+        }
+
+        public int KATNUMARA
+        {
+            get { return katno; }
+            set { katno = Math.Abs(value); }
+
+        }
+        public double ALAN
+        {
+            get { return alan; }
+            set { alan = Math.Abs(value); }
+        }
+
+
+    }
+}
